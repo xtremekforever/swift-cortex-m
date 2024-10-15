65267545828f31bb35b83d0c8918cedfe227ef7b
Jesse L. Zamora <xtremekforever@gmail.com>
2024-10-15 03:18:37 -0400

    Add SysTick.setState helper to enable and disable the peripheral

---
+++ Sources/CortexM/EnabledDisabled.swift
@@ -0,0 +1,4 @@
+public enum EnabledDisabled: UInt32 {
+    case disabled = 0
+    case enabled = 1
+}
--- Sources/CortexM/Extensions/Systick+Helpers.swift
+++ Sources/CortexM/Extensions/Systick+Helpers.swift
@@ -1,7 +1,23 @@
 extension SysTick {
+
+    /// Set the state of the systick counter register.
+    ///
+    /// The systick peripheral can either be in a disabled or enabled state.
+    /// This helper makes it easy to set the state for readability and understanding of the code.
+    /// Note that the systick peripheral may need to be configured beforehand!
+    ///
+    /// - Parameter state: `.enabled` to enable the systick peripheral, `.disabled` to disable it.
+    ///
+    public func setState(_ state: EnabledDisabled) {
+        systick.csr.modify { rw in
+            rw.raw.enable = state.rawValue
+        }
+    }
+
     /// Delay processor by using systick register as a tick source.
     ///
-    /// This assumes that `systick` is initialized, configured, and in the global scope.
+    /// This assumes that `systick` is _configured and enabled_. Otherwise, the method
+    /// will stay locked forever!!
     ///
     /// - Parameter ticks: number of systick "ticks" to delay by.
     ///
