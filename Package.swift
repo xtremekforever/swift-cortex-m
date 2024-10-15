86eadc4d98943840aa9f77fcd47f1096f72f35bb
Jesse L. Zamora <xtremekforever@gmail.com>
2024-10-15 03:00:58 -0400

    Fix module and file names and paths

--- Package.swift
+++ Package.swift
@@ -8,7 +8,8 @@ let package = Package(
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
-            name: "CortexM", targets: ["Registers"])
+            name: "CortexM", targets: ["CortexM"]
+        )
     ],
     dependencies: [
         .package(url: "https://github.com/apple/swift-mmio", branch: "swift-embedded-examples")
@@ -17,7 +18,7 @@ let package = Package(
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "Registers",
+            name: "CortexM",
             dependencies: [
                 .product(name: "MMIO", package: "swift-mmio")
             ],
@@ -27,7 +28,7 @@ let package = Package(
         ),
         .testTarget(
             name: "Tests",
-            dependencies: ["Registers"]
+            dependencies: ["CortexM"]
         ),
     ]
 )
